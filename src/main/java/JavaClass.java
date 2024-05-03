d86e279e81a27df6064fb7e6864de1a6555aa602
user <user>
2024-05-03 16:58:54 +0200

    Test Project

--- src/main/java/JavaClass.java
+++ src/main/java/JavaClass.java
@@ -1,6 +1,10 @@
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class JavaClass {
+    static Logger logger = Logger.getLogger(JavaClass.class.getName());
     public static void main(String[] args) {
+        logger.log(Level.INFO, "Log:: Bonjour, Bienvenue !");
         System.out.println("Bonjour, Bienvenue !");
     }
 }
